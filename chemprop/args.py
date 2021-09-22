a21e5084ce24bc7b652aef249a8589379d4bfda4
Chas <charlesjmcgill@gmail.com>
2021-09-21 22:14:03 -0400

    Hyperopt checkpoint and parallel functionality
    Formatting bug fix
    
    arguments bugfix
    
    Bugfix for the arguments bugfix
    
    Added readme and additional trial loading to avoid missing parallel jobs
    
    Add the ability to include manual training instances in hyperopt.
    
    Bug fix for temporary checkpoint directory
    
    Updates to logging, readme, and default save location
    
    More logging
    
    Whitespace formatting after commas

--- chemprop/args.py
+++ chemprop/args.py
@@ -590,7 +590,7 @@ class TrainArgs(CommonArgs):
     def process_args(self) -> None:
         super(TrainArgs, self).process_args()
 
-        global temp_dir  # Prevents the temporary directory from being deleted upon function return
+        global temp_save_dir  # Prevents the temporary directory from being deleted upon function return
 
         # Change the default number_of_molecules value to 2 if self.reaction_solvent is True
         if self.reaction_solvent is True and not self.number_of_molecules == 2:
@@ -612,8 +612,8 @@ class TrainArgs(CommonArgs):
 
         # Create temporary directory as save directory if not provided
         if self.save_dir is None:
-            temp_dir = TemporaryDirectory()
-            self.save_dir = temp_dir.name
+            temp_save_dir = TemporaryDirectory()
+            self.save_dir = temp_save_dir.name
 
         # Fix ensemble size if loading checkpoints
         if self.checkpoint_paths is not None and len(self.checkpoint_paths) > 0:
@@ -816,6 +816,24 @@ class HyperoptArgs(TrainArgs):
     """Path to :code:`.json` file where best hyperparameter settings will be written."""
     log_dir: str = None
     """(Optional) Path to a directory where all results of the hyperparameter optimization will be written."""
+    hyperopt_checkpoint_dir: str = None
+    """Path to a directory where hyperopt completed trial data is stored. Hyperopt job will include these trials if restarted.
+    Can also be used to run multiple instances in parallel if they share the same checkpoint directory."""
+    startup_random_iters: int = 10
+    """The initial number of trials that will be randomly specified before TPE algorithm is used to select the rest."""
+    manual_trial_dirs: List[str] = None
+    """Paths to save directories for manually trained models in the same search space as the hyperparameter search.
+    Results will be considered as part of the trial history of the hyperparameter search."""
+
+
+    def process_args(self) -> None:
+        super(HyperoptArgs, self).process_args()
+
+        # Assign log and checkpoint directories if none provided
+        if self.log_dir is None:
+            self.log_dir = self.save_dir
+        if self.hyperopt_checkpoint_dir is None:
+            self.hyperopt_checkpoint_dir = self.log_dir
 
 
 class SklearnTrainArgs(TrainArgs):
