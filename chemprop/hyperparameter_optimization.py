ec0bf0edf6e65fe0186d5c36efd63795b37f6994
Yunsie Chung <yunsie@mit.edu>
2022-03-22 17:35:56 -0400

    Modify hyperparam opt space

--- chemprop/hyperparameter_optimization.py
+++ chemprop/hyperparameter_optimization.py
@@ -19,12 +19,18 @@ from chemprop.hyperopt_utils import merge_trials, load_trials, save_trials, get_
 
 
 SPACE = {
-    'hidden_size': hp.quniform('hidden_size', low=300, high=2400, q=100),
-    'depth': hp.quniform('depth', low=2, high=6, q=1),
-    'dropout': hp.quniform('dropout', low=0.0, high=0.4, q=0.05),
-    'ffn_num_layers': hp.quniform('ffn_num_layers', low=1, high=3, q=1)
+    'hidden_size': hp.quniform('hidden_size', low=200, high=1200, q=100),
+    'depth': hp.quniform('depth', low=2, high=7, q=1),
+    'dropout': hp.quniform('dropout', low=0.0, high=0.3, q=0.1),
+    'ffn_num_layers': hp.quniform('ffn_num_layers', low=3, high=8, q=1),
+    'ffn_hidden_size': hp.quniform('ffn_hidden_size', low=200, high=2000, q=200),
+    'warmup_epochs': hp.quniform('warmup_epochs', low=0, high=6, q=1),
+    'batch_size': hp.quniform('batch_size', low=10, high=100, q=10),
+    'init_lr': hp.loguniform('init_lr', low=np.log(1e-7), high=np.log(1e-3)),
+    'max_lr': hp.loguniform('max_lr', low=np.log(1e-7), high=np.log(1e-3)),
+    'final_lr': hp.loguniform('final_lr', low=np.log(1e-8), high=np.log(1e-4)),
 }
-INT_KEYS = ['hidden_size', 'depth', 'ffn_num_layers']
+INT_KEYS = ['hidden_size', 'depth', 'ffn_num_layers', 'ffn_hidden_size', 'warmup_epochs', 'batch_size']
 
 
 @timeit(logger_name=HYPEROPT_LOGGER_NAME)
